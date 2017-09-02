b909fe6c184143faec45a83a873ed9f85acaaae5
Web Development Labs <webdevlabs@users.noreply.github.com>
2017-09-02 11:49:36 +0300

    Add files via upload

--- test/index.php
+++ test/index.php
@@ -2,9 +2,10 @@
 define('BASE_URL', (isset($_SERVER['HTTPS'])?'https':'http').'://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));
 define('ROOT_DIR', dirname(__FILE__));
 
-include '../WebPHPack.php';
+//include '../src/WebPHPack.php';
+require_once "../vendor/autoload.php";
 $htmlsource = file_get_contents('bootstrap.html');
-$webphpack = new WebPHPack($htmlsource);
+$webphpack = new \Phreak\WebPHPack($htmlsource);
 $webphpack->matchString = 'assets/';
 $webphpack->jsPath = ROOT_DIR.'/assets/js';
 $webphpack->cssPath = ROOT_DIR.'/assets/css';
